1a6b44c5951e3c8b08510048f4b8f0bb30022941
Jordan Byron <jordan.byron@gmail.com>
2013-08-02 14:58:59 -0400

    Remove old js assets and cocoon proposal form

--- app/decorators/proposal_decorator.rb
+++ app/decorators/proposal_decorator.rb
@@ -12,40 +12,4 @@ class ProposalDecorator < Draper::Decorator
   def list
     (model.rows + model.groups).sort_by(&:position)
   end
-
-  def add_row_button(f, options={})
-    add_item_button f, :rows, options
-  end
-
-  def add_group_button(f, options={})
-    add_item_button f, :groups, options
-  end
-
-  def render_items(f)
-    list.map do |list_item|
-      if Row === list_item
-        f.fields_for :rows, list_item do |row|
-          h.render "row_fields", f: row
-        end
-      else
-        f.fields_for :groups, list_item do |group|
-          h.render "group_fields", f: group
-        end
-      end
-    end.join("\n").html_safe
-  end
-
-  private
-
-  def add_item_button(f, type, options)
-    h.link_to_add_association(
-      options.delete(:name) || "+ #{type.to_s.singularize.capitalize}",
-      f, type, {
-        class: 'btn btn-small',
-        'data-association-insertion-node'   => '#list',
-        'data-association-insertion-method' => 'append',
-        force_non_association_create: true
-      }.merge(options)
-    )
-  end
 end
