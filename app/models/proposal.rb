83894799860bedc442372fc2dd42d8ba3b4a72bd
Jordan Byron <jordan.byron@gmail.com>
2013-08-17 09:50:11 -0400

    Sane defaults for proposal numbers / revision

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,6 +1,8 @@
 class Proposal < ActiveRecord::Base
   default_scope { where(account_id: Account.current_id) }
 
+  before_create :next_proposal_number
+
   has_many :groups, autosave: true
   has_many :rows,   autosave: true
 
@@ -8,4 +10,11 @@ class Proposal < ActiveRecord::Base
   belongs_to :updated_by, class_name: 'User'
 
   belongs_to :proposal_status
+
+  private
+
+  def next_proposal_number
+    self.proposal_number ||= (Proposal.maximum("proposal_number") || 0) + 1
+    self.revision        ||= 0
+  end
 end
